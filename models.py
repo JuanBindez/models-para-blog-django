63403bb6049ede91b93165cf6e853077cdb44235
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-02-14 10:21:49 -0300

    Update and rename screenLR.py to models.py

---
+++ models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=100)
+    sub_titulo = models.CharField(max_length=200,null=True)
+    data_do_post = models.DateField()
+    conteudo = models.TextField()
+
+    def __str__(self):
+        return self.titulo
