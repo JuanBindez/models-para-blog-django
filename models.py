86f62f1be7da281c2121c25424d634af5dd3f6af
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-02-27 11:00:21 -0300

    Update models.py

--- models.py
+++ models.py
@@ -6,7 +6,9 @@ class Post(models.Model):
     sub_titulo = models.CharField(max_length=200)
     data_do_post = models.DateField()
     conteudo = models.TextField()
-    imagem = models.ImageField(upload_to='posts')
+    imagem1 = models.ImageField(upload_to='posts')
+    imagem2 = models.ImageField(upload_to='posts')
+    imagem3 = models.ImageField(upload_to='posts')
 
     def __str__(self):
         return self.titulo
