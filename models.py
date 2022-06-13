a56aebaf78585ec45f410753993f5932fd56f4ed
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-06-13 12:18:33 -0300

    Update models.py

--- models.py
+++ models.py
@@ -9,9 +9,9 @@ class Post(models.Model):
     conteudo_2 = models.TextField(null=True, blank=True)
     conteudo_3 = models.TextField(null=True, blank=True)
     conteudo_4 = models.TextField(null=True, blank=True)
-    imagem1 = models.ImageField(upload_to='posts')
-    imagem2 = models.ImageField(upload_to='posts', null=True, blank=True)
-    imagem3 = models.ImageField(upload_to='posts', null=True, blank=True)
+    imagem_1 = models.ImageField(upload_to='posts')
+    imagem_2 = models.ImageField(upload_to='posts', null=True, blank=True)
+    imagem_3 = models.ImageField(upload_to='posts', null=True, blank=True)
 
     def __str__(self):
         return self.titulo
