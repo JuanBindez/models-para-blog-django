e9370e37e222663048d8dec34d72147199da9aaf
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-06-13 12:31:06 -0300

    Update models.py

--- models.py
+++ models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    titulo = models.CharField(max_length=100)
-    sub_titulo = models.CharField(max_length=200)
-    data_do_post = models.DateField()
-    conteudo = models.TextField()
+    titulo = models.CharField(max_length=100)#não permitido deixar em branco
+    sub_titulo = models.CharField(max_length=200)#não permitido deixar em branco
+    data_do_post = models.DateField()#não permitido deixar em branco
+    conteudo = models.TextField()#não permitido deixar em branco
     conteudo_2 = models.TextField(null=True, blank=True)
     conteudo_3 = models.TextField(null=True, blank=True)
     conteudo_4 = models.TextField(null=True, blank=True)
-    imagem_1 = models.ImageField(upload_to='posts')
+    imagem_1 = models.ImageField(upload_to='posts')#não permitido deixar em branco
     imagem_2 = models.ImageField(upload_to='posts', null=True, blank=True)
     imagem_3 = models.ImageField(upload_to='posts', null=True, blank=True)
 
